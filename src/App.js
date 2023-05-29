ef6632fbd1e9c28e2401955e04524da11c2879a6
Gclean <ginogallo74@yahoo.com>
2023-05-29 14:39:52 -0400

    Updated app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { Component } from "react";
 import Overview from "./components/Overview";
 
-function App() {
-  return <Overview />;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputVal: [],
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    e.preventDefault();
+
+    const data = new FormData(e.target);
+    const value = data.get("input");
+    if (value === "") return;
+
+    this.setState({
+      inputVal: [...this.state.inputVal, value],
+    });
+    e.target.reset();
+  }
+
+  render() {
+    const { input } = this.state.inputVal;
+
+    return (
+      <>
+        <form onSubmit={this.handleClick}>
+          <label htmlFor="input">Input</label>
+          <input id="input" name="input" type="text"></input>
+          <button type="submit">Submit</button>
+        </form>
+        <Overview input={this.state.inputVal} />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,40 +1,15 @@
 import React, { Component } from "react";
 
-class Overview extends Component {
-  constructor() {
-    super();
+const Overview = props => {
+  const inputs = props.input;
 
-    this.state = {
-      inputVal: [],
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(e) {
-    e.preventDefault();
-
-    const data = new FormData(e.target);
-    const value = data.get("input");
-    if (value === "") return;
-
-    this.setState({
-      inputVal: [...this.state.inputVal, value],
-    });
-    e.target.reset();
-  }
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleClick}>
-          <label htmlFor="input">Input</label>
-          <input id="input" name="input" type="text"></input>
-          <button>Submit</button>
-        </form>
-        <div> {` ${this.state.inputVal}`}</div>
-      </>
-    );
-  }
-}
+  return (
+    <ul>
+      {inputs.map(input => {
+        return <li key={input}>{input}</li>;
+      })}
+    </ul>
+  );
+};
 
 export default Overview;
